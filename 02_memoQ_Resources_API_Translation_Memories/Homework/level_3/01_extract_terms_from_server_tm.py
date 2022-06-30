4cece537253797e50de22c0addeda1f4556e827a
Christian Wecke <60540663+cwecke90@users.noreply.github.com>
2022-06-30 08:52:34 +0200

    complete homework module 2

--- 02_memoQ_Resources_API_Translation_Memories/Homework/level_3/01_extract_terms_from_server_tm.py
+++ 02_memoQ_Resources_API_Translation_Memories/Homework/level_3/01_extract_terms_from_server_tm.py
@@ -28,15 +28,37 @@ sample_guid = '61359674-9c32-49b6-bb2b-c84b587e0d09'
 # Remember that spacy language packages are not consistently names. This applies to German packages in particular!
 
 def load_nlp(lang, model_size='lg'):
-    nlp = None
-    # TODO: Write your code here! Feel free to delete this comment!
+    if lang == "en":
+        model_name = f"{lang}_core_web_{model_size}"
+    else:
+        model_name = f"{lang}_core_news_{model_size}"
+    nlp = spacy.load(model_name)
     return nlp
 
 
 def extract_terms_from_memoq_tm(tm_guid):
     term_candidates = set()
     lang_map = {'eng-US': 'en', 'ger-DE': 'de'}
-    # TODO: Write your code here! Feel free to delete this comment!
+    tm_data = get_tm(tm_guid)
+    src_lang = tm_data.get("SourceLangCode")
+    trg_lang = tm_data.get("TargetLangCode")
+    nlp_obj_src = load_nlp(lang_map[src_lang])
+    nlp_obj_trg = load_nlp(lang_map[trg_lang])
+    tus = get_tus(tm_guid)
+    for segments in tus:
+        if "ErrorCode" not in segments:
+            source_segment = segments.get("SourceSegment")
+            source_segment = re.sub(r'<seg>(.+?)<\/seg>', r'\g<1>', source_segment)
+            target_segment = segments.get("TargetSegment")
+            target_segment = re.sub(r'<seg>(.+?)<\/seg>', r'\g<1>', target_segment)
+            doc_src = nlp_obj_src(source_segment)
+            for ne_src in doc_src:
+                if ne_src.ent_type_:
+                    term_candidates.add(ne_src.text)
+            doc_trg = nlp_obj_trg(target_segment)
+            for ne_trg in doc_trg:
+                if ne_trg.ent_type_:
+                    term_candidates.add(ne_trg.text)
     return term_candidates
 
 
@@ -44,4 +66,4 @@ def extract_terms_from_memoq_tm(tm_guid):
 # Test your function! Use the function "extract_terms_from_memoq_tm"
 # Print out the results!
 
-# TODO: Write your code here! Feel free to delete this comment!
+print(extract_terms_from_memoq_tm(sample_guid))
